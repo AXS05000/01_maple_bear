85e757feb3d9a1901535d36aa6f9a1b0811d1863
AXS05000 <axssxs00@gmail.com>
2024-02-26 12:01:12 -0300

    V - 26/02/2024 - 12:01

--- usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('usuario/', include('django.contrib.auth.urls')),
-    path('login/', views.login_view, name='login'),
-]
