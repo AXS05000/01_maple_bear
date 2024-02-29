390d902c4d12e259b937443b7d308f9e87aedf61
AXS05000 <axssxs00@gmail.com>
2024-02-29 20:36:45 -0300

    V - 29/02/2024 - 20:36

--- projeto_maple/wsgi.py
+++ projeto_maple/wsgi.py
@@ -1,8 +1,16 @@
+"""
+WSGI config for projeto_maple project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_maple.settings')
 
 application = get_wsgi_application()
