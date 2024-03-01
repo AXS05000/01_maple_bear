732dedaaf159df4530d6ad40607b6cc5c815b5b8
AXS05000 <axssxs00@gmail.com>
2024-03-01 15:56:57 -0300

    V - 01/03/2024 - 15:56

--- projeto_maple/settings.py
+++ projeto_maple/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    # Adicione outras configurações do DRF aqui, se necessário
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
